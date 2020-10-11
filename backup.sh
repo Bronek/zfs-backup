a46ba0562466ecf4b27df99a2a50408216b43a52
Bronek Kozicki <brok@incorrekt.com>
2020-10-11 21:16:08 +0100

    Improved usage
    Do not show "not a snapshot" when dataset not found
    Show zfs commands executed

--- backup.sh
+++ backup.sh
@@ -6,6 +6,7 @@ CONFIRM=0
 DRYRUN=0
 VERBOSE=0
 
+# parse options
 while [[ $# -gt 0 ]]; do
   key="$1"
   case $key in
@@ -28,13 +29,27 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
+# -y and -n are clashing, -n wins
 if [[ $DRYRUN -gt 0 ]]; then
   CONFIRM=0
 fi
 
 help() {
-  >&2 echo "Usage:"
-  >&2 echo "$0 [-y|--yes][-n|--dry-run][-v|--verbose] source destination [@snapshot]"
+  >&2 echo "Usage: $0 [-y|--yes][-n|--dry-run][-v|--verbose] source destination [@snapshot]"
+  >&2 echo
+  >&2 echo "Copy last (or selected) snapshot from source to destination ZFS filesystem"
+  >&2 echo "Will traverse all nested filesystems and volumes like 'zfs send -R' does"
+  >&2 echo
+  >&2 echo "Parameters:"
+  >&2 echo "  -d, --dry-run  No-op. Performs 'zfs send -n ... | cat'. Implied if -y is not set"
+  >&2 echo "  -y, --yes      Do perform actual 'zfs send ... | zfs receive'"
+  >&2 echo "  -v, --verbose  Show progress when sending data, i.e. 'zfs send -v ...'"
+  >&2 echo
+  >&2 echo "Notes:"
+  >&2 echo "  - @snapshot must start with @"
+  >&2 echo "  - destination filesystem will be rolledback before receive"
+  >&2 echo "  - destination must be a locally imported ZFS pool"
+  >&2 echo "  - execution will abort on first error"
   exit 1
 }
 
@@ -44,7 +59,10 @@ read_type() {
 
 check_snapshot() {
   TYPE=$(read_type "${1}${2}")
-  if [ "${TYPE}" != "snapshot" ]; then
+  if [[ -z "$TYPE" ]]; then
+    exit 1
+  fi
+  if [ "$TYPE" != "snapshot" ]; then
     >&2 echo "'${1}${2}' is not a snapshot"
     exit 1
   fi
@@ -52,17 +70,18 @@ check_snapshot() {
 
 find_snapshot() {
   RES="@"$(zfs list -t snapshot ${1} -o name -H | tail -1 | cut -d@ -f2)
-  if [ "${RES}" == "@" ]; then
+  if [ "$RES" == "@" ]; then
     >&2 echo "'${1}' has no snapshots"
     exit 1
   fi
-  echo "${RES}"
+  echo "$RES"
 }
 
 SOURCE=
 DEST=
 SNAP=
 
+# read source, destination and optional snapshot name
 if [[ ${#POSITIONAL[@]} -lt 2 ]]; then
   help
 else
@@ -79,18 +98,17 @@ else
 fi
 
 OBJECTS=$(zfs list -t volume,filesystem -o name -H -r ${SOURCE})
+dryrun=""
+if [[ $CONFIRM -lt 1 ]]; then
+  dryrun="-n"
+fi
+
+verbose=""
+if [[ $VERBOSE -eq 1 ]]; then
+  verbose="-v"
+fi
 
 for i in ${OBJECTS}; do
-  dryrun=""
-  if [[ $CONFIRM -lt 1 ]]; then
-    dryrun="-n"
-  fi
-
-  verbose=""
-  if [[ $VERBOSE -eq 1 ]]; then
-    verbose="-v"
-  fi
-
   now=${SNAP}
   if [[ -z "$now" ]]; then
     now=$(find_snapshot $i)
@@ -101,10 +119,12 @@ for i in ${OBJECTS}; do
   if [[ $CONFIRM -eq 1 ]]; then
     recv="zfs receive $j"
   fi
+  show="zfs receive $j"
 
   TYPE=$(read_type $j)
   if [[ -z "$TYPE" ]]; then
     echo "! creating '$j'"
+    echo "# zfs send $dryrun $verbose -R -w ${i}${now} | ${recv[@]}"
     zfs send $dryrun $verbose -R -w "${i}${now}" | ${recv[@]}
   else
     old=$(find_snapshot $j)
@@ -112,9 +132,11 @@ for i in ${OBJECTS}; do
       echo ". '${j}' is up to date"
     else
       echo "+ updating '$j'"
+      echo "# zfs rollback ${j}${old}"
       if [[ $CONFIRM -eq 1 ]]; then
         zfs rollback "${j}${old}"
       fi
+      echo "# zfs send $dryrun $verbose -p -w -I $old ${i}${now} | ${show[@]}"
       zfs send $dryrun $verbose -p -w -I $old "${i}${now}" | ${recv[@]}
     fi
   fi
